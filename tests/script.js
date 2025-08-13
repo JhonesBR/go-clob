5164d47199449446d68462d82f35a4c6ce12a775
João Vitor Melo <joaovitor.oliveira.melo@gmail.com>
2025-08-13 18:58:13 -0300

    test: refactor tests into functions

--- tests/script.js
+++ tests/script.js
@@ -5,50 +5,25 @@ export const options = {
 };
 
 const BASE_URL = "http://localhost:8000/v1"
+const HEADERS = { headers: { 'Content-Type': 'application/json' } };
 
 export default function () {
   // Create two accounts
-  let name = "Account 1"
-  let res = http.post(`${BASE_URL}/accounts`, JSON.stringify({ name }), { headers: { 'Content-Type': 'application/json' } });
-  check(res, { "Account 1 created": (res) => res.status === 201 });
-  let account_id_1 = res.json().id;
-
-  name = "Account 2"
-  res = http.post(`${BASE_URL}/accounts`, JSON.stringify({ name }), { headers: { 'Content-Type': 'application/json' } });
-  check(res, { "Account 2 created": (res) => res.status === 201 });
-  let account_id_2 = res.json().id;
+  let account_id_1 = createAccount("Account 1");
+  let account_id_2 = createAccount("Account 2");
 
   // Charge account 1 with BTC
-  res = http.post(`${BASE_URL}/accounts/${account_id_1}/charge`, JSON.stringify({ amount: 100, asset_code: "BTC" }), { headers: { 'Content-Type': 'application/json' } });
-  check(res, { "Charged account 1 with BTC": (res) => res.status === 200 });
+  chargeAccount(account_id_1, 100, "BTC");
 
   // Charge account 2 with 500.000 BRL
-  res = http.post(`${BASE_URL}/accounts/${account_id_2}/charge`, JSON.stringify({ amount: 500000, asset_code: "BRL" }), { headers: { 'Content-Type': 'application/json' } });
-  check(res, { "Charged account 2 with BRL": (res) => res.status === 200 });
+  chargeAccount(account_id_2, 500000, "BRL");
 
   // Create multiple sell and buy orders
   for (let i = 0; i < 10; i++) {
-    let payload = {
-      "account_id": account_id_1,
-      "asset_code": "BTC",
-      "quantity": 1,
-      "price": 100,
-      "order_type": "sell",
-    }
-    res = http.post(`${BASE_URL}/order_book`, JSON.stringify(payload), { headers: { 'Content-Type': 'application/json' } });
-    check(res, { "Created first sell order": (res) => res.status === 204 });
+    createOrder(account_id_1, "BTC", 1, 100, "sell");
   }
-
   for (let i = 0; i < 10; i++) {
-    let payload = {
-      "account_id": account_id_2,
-      "asset_code": "BTC",
-      "quantity": 1,
-      "price": 100,
-      "order_type": "buy",
-    }
-    res = http.post(`${BASE_URL}/order_book`, JSON.stringify(payload), { headers: { 'Content-Type': 'application/json' } });
-    check(res, { "Created first buy order": (res) => res.status === 204 });
+    createOrder(account_id_2, "BTC", 1, 100, "buy");
   }
 
   // Check account balances
@@ -56,33 +31,34 @@ export default function () {
   checkAccountBalances(account_id_2, "10", "499000");
 
   // Create a big sell order
-  let payload = {
-    "account_id": account_id_1,
-    "asset_code": "BTC",
-    "quantity": 50,
-    "price": 100,
-    "order_type": "sell",
-  }
-  res = http.post(`${BASE_URL}/order_book`, JSON.stringify(payload), { headers: { 'Content-Type': 'application/json' } });
-  check(res, { "Created big sell order": (res) => res.status === 204 });
+  createOrder(account_id_1, "BTC", 50, 100, "sell");
 
   // Buy then in small orders
   for (let i = 0; i < 100; i++) {
-    let payload = {
-      "account_id": account_id_2,
-      "asset_code": "BTC",
-      "quantity": 0.5,
-      "price": 100,
-      "order_type": "buy",
-    }
-    res = http.post(`${BASE_URL}/order_book`, JSON.stringify(payload), { headers: { 'Content-Type': 'application/json' } });
-    check(res, { "Created small buy order": (res) => res.status === 204 });
+    createOrder(account_id_2, "BTC", 0.5, 100, "buy");
   }
 
   // Check account balances
   checkAccountBalances(account_id_1, "40", "6000");
   checkAccountBalances(account_id_2, "60", "494000");
 };
+
+function createAccount(name) {
+  let res = http.post(`${BASE_URL}/accounts`, JSON.stringify({ name }), HEADERS);
+  check(res, { [`Account ${name} created`]: (res) => res.status === 201 });
+  return res.json().id;
+}
+
+function chargeAccount(account_id, amount, asset_code) {
+  let res = http.post(`${BASE_URL}/accounts/${account_id}/charge`, JSON.stringify({ amount, asset_code }), HEADERS);
+  check(res, { [`Charged account ${account_id} with ${asset_code}`]: (res) => res.status === 200 });
+}
+
+function createOrder(account_id, asset_code, quantity, price, order_type) {
+  let payload = { account_id, asset_code, quantity, price, order_type };
+  let res = http.post(`${BASE_URL}/order_book`, JSON.stringify(payload), HEADERS);
+  check(res, { [`Created ${order_type} order`]: (res) => res.status === 204 });
+}
 
 function checkAccountBalances(account_id, expected_btc_balance, expected_brl_balance) {
   let res = http.get(`${BASE_URL}/accounts/${account_id}`);
